5164b268f75c44ccfe68651a42b8b390df14f31d
YiXi <10053830@philasd.org>
2025-12-15 18:37:58 -0500

    (feat kitbot)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,6 +9,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final int LEFT_LEADER_ID = 0;
+  public static final int LEFT_FOLLOWER_ID = 1;
+  public static final int RIGHT_LEADER_ID = 0;
+  public static final int RIGHT_FOLLOWER_ID = 1;
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
