ad5ec82a6732459f45eff842ee1ea6aa05beb5a5
Kyle <revised980@gmail.com>
2025-12-15 18:46:20 -0500

    feat kitbot

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  private final SparkMax leftLeader;
+  private final SparkMax leftFollower;
+  private final SparkMax rightLeader;
+  private final SparkMax rightFollower;
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {
+    leftLeader = new SparkMax(LEFT_LEADER_ID, MotorType.kBrushed);
+    leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MotorType.kBrushed);
+    rightLeader = new SparkMax(RIGHT_LEADER_ID, MotorType.kBrushed);
+    rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+
+    leftLeader.setCANTimeout(250);
+    rightLeader.setCANTimeout(250);
+    leftFollower.setCANTimeout(250);
+    rightFollower.setCANTimeout(250);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
