86f2af13df9271e5197a0c5041ab407089c8f7a1
Sarah <lunyuhua@gmail.com>
2025-12-15 20:20:43 -0500

    feat tankdrive

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -1,57 +1,60 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkLowLevel.MoterType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import frc.robot.Constants.OperatorConstants; 
 
 public class DriveTrainSubsystem extends SubsystemBase {
-
-  leftLeader = new SparkMax(LEFT_LEADER_ID, MoterType.kBrushed);
-  leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MoterType.kBrushed);
-  rightLeader = new SparkMax(RIGHT_LEADER_ID, MoterType.kBrushed);
-  rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MoterType.kBrushed);
 
   private final SparkMax leftLeader;
   private final SparkMax leftFollower;
   private final SparkMax rightLeader;
   private final SparkMax rightFollower;
 
+  private final DifferentialDrive drive;
+
   /** Creates a new DriveTrainSubsystem. */
-  public DriveTrainSubsystem() {}
+  public DriveTrainSubsystem() {
 
+  leftLeader = new SparkMax(0, MotorType.kBrushed);
+  leftFollower = new SparkMax(1, MotorType.kBrushed);
+  rightLeader = new SparkMax(2, MotorType.kBrushed);
+  rightFollower = new SparkMax(3, MotorType.kBrushed);
 
+  leftLeader.setCANTimeout(250);
+  leftFollower.setCANTimeout(250);
+  rightLeader.setCANTimeout(250);
+  rightFollower.setCANTimeout(250);
+  
+  SparkMaxConfig config = new SparkMaxConfig();
+  config.voltageCompensation(12);
+  config.smartCurrentLimit(OperatorConstants.DRIVE_MOTOR_CURRENT_LIMIT);
+  
+  config.follow(leftLeader);
+  leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  config.follow(rightLeader);
+  rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  
+  config.inverted(true);
+  leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-  /**
-   * DriveTrain command factory method.
-   *
-   * @return a command
-   */
-  public Command DriveTrainMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
+  drive = new DifferentialDrive(leftMotors, rightMotors);
 
-  /**
-   * An DriveTrain method querying a boolean state of the subsystem (for DriveTrain, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean DriveTrainCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  public void tankDrive(double moveSpeed, double rotateSpeed) {
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    drive.tankDrive(moveSpeed, rotateSpeed);
+
   }
 }
