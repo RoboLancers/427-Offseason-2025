1fae28a4b2571cdb38d3d0e19af83a923fb0465e
Connor Glazer <deerliver.contact@gmail.com>
2025-12-15 19:46:38 -0500

    2nd Day of work, added most things but the actual code for the differental drive.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,10 +12,10 @@ public final class Constants {
   public static class OperatorConstants {
 
     //Motor IDs
-    public static final int LEFT_LEADER_ID = 0;
-    public static final int LEFT_FOLLOWER_ID = 1;
-    public static final int RIGHT_LEADER_ID = 0;
-    public static final int RIGHT_FOLLOWER_ID = 1;
+    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int RIGHT_LEADER_ID = 3;
+    public static final int RIGHT_FOLLOWER_ID = 4;
 
     public static final int kDriverControllerPort = 0;
   }
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -6,32 +6,63 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * I AM CONFUSED AND CONFUDLED.
+ * 
+ * ??????????????????????????????????????
+ * 
+ * GOOD LORD I NEED GOOGLE
+ */
 public class DrivetrainSubsystem extends SubsystemBase {
-  //create brushed motors for drive
-  private SparkMax leftLeader;
-  private SparkMax leftFollower;
-  private SparkMax rightLeader;
-  private SparkMax rightFollower;
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private final SparkMax leftLeader;
+  private final SparkMax leftFollower;
+  private final SparkMax rightLeader;
+  private final SparkMax rightFollower;
 
-  leftLeader = new SparkMax(LEFT_LEADER_ID, MotorType.kBrushed);
-  leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MotorType.kBrushed);
-  rightLeader = new SparkMax(RIGHT_LEADER_ID, MotorType.kBrushed);
-  rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+  // Set can timeout. Because this project only sets parameters once on construction, the timeout can be long without blocking robot operation. Code which sets or gets parameters during operation may need a shorter timeout.
+  leftLeader.setCANTimeout(250);
+  rightLeader.setCANTimeout(250);
+  leftFollower.setCANTimeout(250);
+  rightFollower.setCANTimeout(250);
 
-  // Set can timeout. Because this project only sets parameters once on
-    // construction, the timeout can be long without blocking robot operation. Code
-    // which sets or gets parameters during operation may need a shorter timeout.
+  //Config for voltage compensation or smth idfk
+  SparkMaxConfig config = new SparkMaxConfig();
+  config.voltageCompensation(12);
+  config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
 
-  /** Creates a new DrivetrainSubsystem. */
   public DrivetrainSubsystem() {
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
+    // Talons
+    leftLeader = new SparkMax(0, MotorType.kBrushed);
+    leftFollower = new SparkMax(1, MotorType.kBrushed);
+    rightLeader = new SparkMax(2, MotorType.kBrushed);
+    rightFollower = new SparkMax(3, MotorType.kBrushed);
   }
 
+  //
+
+  //X THIS LATER (PREFERABLY TOMMAROW SICNE I NEED THIS DONE REALLY FRIGGEN SOON)
+   private final DifferentialDrive drive;
+   drive = new DifferentialDrive(leftMotors, rightMotors);
+
+   config.follow(leftLeader);
+   leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+   config.follow(rightLeader);
+   rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+   config.disableFollowerMode();
+   rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+   config.inverted(true);
+   leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+   */
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
