628fc1c4ea31e30db88b4f1410ba314bb37d2318
Milana <hermibro7483@gmail.com>
2025-12-15 19:16:04 -0500

    feat kitbot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,16 +9,14 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int LEFT_LEADER_ID = 0;
-    public static final int LEFT_FOLLOWER_ID = 1;
-    public static final int RIGHT_LEADER_ID = 0;
-    public static final int RIGHT_FOLLOWER_ID = 1;
+  public static final class DriveConstants{
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 0;
+    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int RIGHT_LEADER_ID = 3;
+    public static final int RIGHT_FOLLOWER_ID = 4;
     public static final int kDriverControllerPort = 0;
     
     
-  }
-  public static class DriveConstants{
-    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 0;
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -20,7 +20,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      new CommandXboxController(DriveConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
