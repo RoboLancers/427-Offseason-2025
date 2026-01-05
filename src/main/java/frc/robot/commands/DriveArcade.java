034fe96c2b4eab6b491f667d7b77ba6d1afc3096
Connor Glazer <deerliver.contact@gmail.com>
2026-01-05 16:15:19 -0500

    Created/Started DriveArcade command

---
+++ src/main/java/frc/robot/commands/DriveArcade.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.DriveSubsystem;
+import java.util.function.DoubleSupplier; 
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class DriveArcade extends Command {
+  /** Creates a new DriveArcade. */
+
+  private final DoubleSupplier xSpeed;
+  private final DoubleSupplier zRotation;
+  private final DriveSubsystem driveSubsystem;
+
+  this.xSpeed = xSpeed;
+  this.zRotation = zRotation;
+  this.driveSubsystem = driveSubsystem;
+  addRequirements(this.drivetrain);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.driveArcade(xSpeed.getAsDouble(), zRotation.getAsDouble());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
